39b4d34f0b2a35feb22f96c159b7603651b79499
Malik Shahzad Muzaffar <shahzad.malik.muzaffar@cern.ch>
2019-01-25 15:24:43 +0100

    updated version

--- client/cmspkg.py
+++ client/cmspkg.py
@@ -68,7 +68,7 @@ except:
     getstatusoutput("rm -f %s" % tmpfile)
     return sha
 
-cmspkg_tag   = "V00-00-28"
+cmspkg_tag   = "V00-00-29"
 cmspkg_cgi   = 'cgi-bin/cmspkg'
 opts         = None
 cache_dir    = None
